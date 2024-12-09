c24ee371c447b1e8e0f0d502e78f2972b8c56208
DostonbekZaripov <dastandev0110@gmail.com>
2024-12-10 04:16:37 +0500

    The end

---
+++ main.js
@@ -0,0 +1,42 @@
+// 1-masala
+// function arrayMadness(a, b) {
+//     const sumkvadrat = a.reduce((sum, num) => sum + num ** 2, 0);
+//     const sumkub = b.reduce((sum, num) => sum + num ** 3, 0);
+//     return sumkvadrat > sumkub;
+// }
+// console.log(arrayMadness([4, 5, 6], [1, 2, 3]));
+
+// 2-masala
+// function joinElements(array) {
+//     return array.join(","); 
+// }
+// console.log(joinElements(["h", "o", "l", "a"]));
+
+// 3-masala
+// let arr = [1,3,5,7]
+// let count = 0;
+// for(let i=0;i<arr.length;i++){
+//     count +=arr[i]
+// }
+// console.log(count/arr.length);
+
+
+// 4-masala
+// function joinElements(array) {
+//     return array.join(","); 
+// }
+// console.log(joinElements(["I love arrays they are my favorite"]));
+
+
+// 5-masala
+// function joinElements(array) {
+//     return array.join(","); 
+// }
+// console.log(joinElements(["h", "o", "l", "a"]));
+
+// 6-masala
+// function small(array){
+//     return Math.min(...array)
+// }
+// console.log(small([34, 15, 88, 2]));
+// console.log(small([34, -345, -1, 100]));
